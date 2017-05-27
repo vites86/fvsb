167e74838be9579485dd2f3cd48cbf65bb2ede4a
vites86 <vites@outlook.com>
2017-05-27 17:03:57 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Про нас</title>
+  <?php include("blocks/header_links.php") ;?>   
+</head>
+
+<body>
+  <header>
+    <?php include("blocks/header.php") ;?>
+	<script>
+        $(window).load(function(){   
+           document.getElementById("header_about").className      = 'active';
+});
+      </script>
+  </header>
+
+  <div id="breadcrumb">
+    <div class="container">
+      <div class="breadcrumb">
+        <li><a href="index.html">Головна</a></li>
+        <li>Про нас</li>
+      </div>
+    </div>
+  </div>
+
+  <div class="aboutus">
+    <div class="container">
+      <h3><span style="color:grey; margin-bottom:25px;">громадська організація</span><br>
+	      <hr style="margin: 3px 0px 3px 0px !important;">
+          Всеукраїнська Федерація військово-спортивних багатоборств <b>(ВФВСБ)</b>
+	  </h3>
+      <hr>
+      <div class="col-md-7 wow fadeInDown justif" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <img src="images/world.png" class="img-responsive">
+        <h4>Ми за здоровий спосіб життя!</h4>
+        <p>Громадська організація "Всеукраїнська Федерація військово-спортивних багатоборств" <b>(ВФВСБ)</b> створена 8 лютого 2000 року на базі Федерації військово-прикладного спорту Харківської області (зареєстрована 31.01.1997)</p>
+        <p>08.10.2010 року ВФВСБ наказом № 3531 Міністерства України у справах молоді, сім'ї та спорту присвоєно статус <b>«Національної»</b>.</p>
+        <p><img src="images/zaicev.jpg" style="width: 20%; float: left; margin-right: 10px;">У 2005 році ВФВСБ стала одним із засновників Міжнародного Союзу громадських організацій «Розвитку військово - спортивних багатоборств»,&nbsp; &nbsp;президентом
+          &nbsp;якого&nbsp;&nbsp; став <b>Зайцев С.В.</b> (Засновник бойового багатоборства, заслужений тренер України, суддя міжнародної категорії, чорний пояс 10 дан, майстер спорту), першим віце-президентом став <b>Федорцов О.П.</b> - Засновник бойового
+          багатоборства, суддя міжнародної категорії, чорний пояс 8 дан), другим віце-президентом став <b>Самсонов В</b>.</p>
+        <p>
+		<!--<img src="images/logo.png" style="width: 35%; float: right; margin-left: 50px; margin-top:25px;">-->
+		  Основна мета діяльності організації – військово-патріотичне виховання дітей та молоді, пропаганда здорового способу життя, підвищення громадянської
+          активності дітей та молоді, забезпечення їх доступу до участі у державотворчих процесах, залучення дітей та молоді до культурного життя України та сприяння проведенню державних, народних і традиційних свят за їх участю, сприяння розвитку лідерських
+          якостей у дітей та молоді, сприяння підготовці молоді до служби у лавах Збройних Сил України.</p>
+        <p>В даний час ВФВСБ має свої структурні підрозділи в <b>11 областях України</b>.</p>
+        <p>ВФВСБ культивує такі напрямки як:
+          <br> - <em><span style="color:#1bbd36; font-weight: bold;">Бойове двоборство</span></em>
+          <br> - <em><span style="color:#1bbd36;font-weight: bold;">Військово-прикладне семиборство</span></em></p>
+        <p> <b><span>«Бойове двоборствo»</span></b> - є прикладним видом спорту і включає в себе наступні розділи:
+          <br> - <em>Всестильової бій</em>, володіння прийомів самозахисту за допомогою ударної і кидкової техніки з використанням елементів боротьби, боксу та східних єдиноборств (карате та ін.).
+          <br> - <em>Стрільба</em>, володіння вогнепальною зброєю і навичками практичної стрільби></p>
+        <p><em>Всестильової бій</em> - раціональне поєднання ударів руками, ногами, ліктями, колінами, боротьби (кидки, больові і задушливі прийоми)</p>
+        <img src="images/slider/versions.png" style="width: 100%; margin: 5px 0 5px 0;">
+        <p><b>Військово-прикладне семиборствo</b> в себе включає наступні види спорту:
+          <br> - плавання 50 метрів
+          <br> - стрільба з автомата Калашникова
+          <br> - біг на 100 та 3000 м
+          <br> - підтягування на перекладені
+          <br> - подолання смуги перешко,
+          <br> - метання гранат. </p>
+        <p>Спортсмени, які займаються цим видом спорту, мають можливість брати участь:
+          <br> - У поясний атестації міжнародної Федерації
+          <br> - У районних, обласних, національних, міжнародних змаганнях та отримувати відповідні <b>спортивні розряди</b> і <b>звання</b>
+          <br> - У літніх спортивних таборах
+          <br> - У військово-патріотичних іграх
+          <br>
+        </p>
+        <p><img src="images/fedorcov.jpg" style="width: 7%; float: left;margin: 5px 5px 5px 0px">Перший віце-перзидент - Олександр Федорцов, засновник бойового двоборства, суддя міжнародної категорії (чорний пояс 8-й дан).
+          <br> Телефони: (057) 343-67-76, (066) 748-76-81 (мобильный)</p>
+        <p>Центральний офіс Всеукраїнської федерації військово-спортивних багатоборств знаходиться в Україні, м.Харків.</p>
+      </div>
+      <div class="col-md-5 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+        <div class="skill">
+          <h2>Досягнення</h2>
+          <p>ВФВСБ на даний момент підготовлені:</p>
+
+          <div class="progress-wrap">
+            <h3 style="margin-bottom: 35px !important;">Заслужених тренерів України</h3>
+            <div class="progress">
+              <div class="progress-bar  color1" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 18%">
+                <span class="bar-width">9</span>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="progress-wrap">
+            <h4 style="margin-bottom: 35px !important;">Майстрів спорту міжнародного класу</h4>
+            <div class="progress">
+              <div class="progress-bar color2" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                <span class="bar-width">20</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="progress-wrap">
+            <h4>Майстрів спорту України</h4>
+            <div class="progress">
+              <div class="progress-bar color3" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+                <span class="bar-width">200</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="progress-wrap">
+            <h4 style="margin-bottom: 35px !important;">суддів міжнародної категорії</h4>
+            <div class="progress">
+              <div class="progress-bar color4" role="progressbar" aria-valuenow="14" aria-valuemin="0" aria-valuemax="100" style="width: 14%">
+                <span class="bar-width">7</span>
+              </div>
+            </div>
+          </div>
+          <div class="progress-wrap">
+            <h4 style="margin-bottom: 35px !important;">суддів Національної категорії</h4>
+            <div class="progress">
+              <div class="progress-bar color1" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 10%">
+                <span class="bar-width">5</span>
+              </div>
+            </div>
+          </div>
+          <div class="progress-wrap">
+            <h4>суддів 1-3 їх категорій</h4>
+            <div class="progress">
+              <div class="progress-bar color2" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 90%">
+                <span class="bar-width"> > 150</span>
+              </div>
+            </div>
+          </div>
+          <img style="width: 100%; margin-top: 10px;" src="images/military2.jpg">
+          <img style="width: 100%; margin-top: 10px;" src="images/poltava.jpg">
+          <img style="width: 100%; margin-top: 10px;" src="images/cherson.jpg">
+          <img style="width: 100%; margin-top: 10px;" src="images/harkiv.jpg">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="our-team">
+    <div class="container">
+      <h3>Керівництво Федерації</h3>
+      <div class="text-center">
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+          <img src="images/services/2.png" alt="">
+          <h4>Федорцов Олександр Петрович</h4>
+          <p>Віце - Президент Федерації
+            <br> 8 дан
+            <br>Суддя міжнародної категорії </p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+          <img src="images/services/1.png" alt="">
+          <h4>Зацев Сергій Васильович</h4>
+          <p>Президент Федерації
+            <br> 10 дан
+            <br>Майстер спорту України</p>
+        </div>
+        <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms">
+          <img src="images/services/3.jpg" alt="">
+          <h4>John Doe</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing eil sed deiusmod tempor</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <? include("blocks/footer.php"); ?>
+  </footer>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="js/jquery-2.1.1.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.prettyPhoto.js"></script>
+  <script src="js/jquery.isotope.min.js"></script>
+  <script src="js/wow.min.js"></script>
+  <script src="js/functions.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>ВСБ</title>
+  <?php include("blocks/header_links.php") ;?>
+</head>
+
+<body>
+  <header>
+    <?php include("blocks/header.php"); ?>
+      <script>
+        function showStatus() {   
+               var el = document.getElementById('status');
+               var display = getComputedStyle(el).display;
+               el.style.display = display === 'none' ? 'block' : 'none';                     
+            // alert("08.10.2010 року ВФВСБ наказом № 3531 Міністерства України у справах молоді, сім'ї та спорту присвоєно статус <b>«Національної»</b>", this);
+        }
+             
+        $(window).load(function(){   
+           document.getElementById("header_index").className = 'active';          
+      }); 
+</script>
+  </header>
+
+  <section id="main-slider" class="no-margin">
+    <div class="carousel slide">
+      <div class="carousel-inner">
+        <div class="item active" style="background-image: url(images/slider/bg1.jpg)">
+          <div class="container">
+            <div class="row slide-margin">
+              <div class="col-sm-6">
+                <div class="carousel-content" style="text-align:center !important;text-shadow: grey 1px 2px 3px;">
+                  <p class="animation animated-item-2">громадська організація</p>
+                  <h2 class="animation animated-item-1">Всеукраїнська <span>Федерація</span></h2>
+                  <p class="animation animated-item-2"style="color:white !important; ">військово-спортивних багатоборств</p>
+                  <img style="width:45%;" src="images/logo.png" />
+                  <br>
+                  <br>
+                  <span id='status' style="display:none">08.10.2010 року ВФВСБ наказом № 3531 Міністерства України у справах молоді, сім'ї та спорту присвоєно статус <b>«Національної»</b></span>
+                  <input type="button" onclick="showStatus()" value="Статус федерації" class="btn-slide animation animated-item-3 btn btn-success" href="#"></input>
+                </div>
+              </div>
+
+              <div class="col-sm-6 hidden-xs animation animated-item-4">
+                <div class="slider-img">
+                  <img style="margin-top: 50px;" src="images/slider/maine_slide.png" class="img-responsive">
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <!--/.item-->
+      </div>
+      <!--/.carousel-inner-->
+    </div>
+    <!--/.carousel-->
+  </section>
+  <!--/#main-slider-->
+
+  <div class="feature">
+    <div class="container">
+      <div class="text-center">
+        <h1 style="color: #1BBD36; font-weight: bold;margin-top: -20px;">Військово - спортивні багатоборства</h1>
+
+        <div class="col-md-6">
+          <div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+            <!-- <h2>Бойове двоборство:</h2>
+<ul type="none" style="color:black; text-align: left; ">
+<li>- стрільба з пневматичної гвинтівки</li>
+<li>- всестильовий бій <em style="color: grey">(раціональне поєднання ударів руками, ногами, ліктями, колінами, боротьби(кидки, больові і задушливі прийоми)</em></li>
+<li><img style="width: 100%;margin-top: 15px;" src="images/slider/dvoborstvo.png"></li>
+</ul> -->
+            <img style="width: 100%;margin-top: 15px;" src="images/slider/dvoborstvo.png">
+
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+            <h2>Військово-прикладне семиборствo:</h2>
+            <ul type="none" style="color:black; text-align: left; margin-left: 100px;">
+              <li>- плавання 50 метрів</li>
+              <li>- стрільба з автомата Калашникова</li>
+              <li>- біг на 100 та 3000 м</li>
+              <li>- підтягування на перекладені</li>
+              <li>- подолання смуги перешкод</li>
+              <li>- метання гранат</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="about">
+    <div class="container">
+      <div class="col-md-6 wow fadeInDown justif" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <h2>Про нас</h2>
+        <img src="images/world.png" class="img-responsive" />
+        <p>Громадська організація "Всеукраїнська Федерація військово-спортивних багатоборств" <b>(ВФВСБ)</b> створена 8 лютого 2000 року на базі Федерації військово-прикладного спорту Харківської області (зареєстрована 31.01.1997)
+        </p>
+        <p>08.10.2010 року ВФВСБ наказом № 3531 Міністерства України у справах молоді, сім'ї та спорту присвоєно статус <b>«Національної»</b>.
+        </p>
+        <p>У 2005 році ВФВСБ стала одним із засновників Міжнародного Союзу громадських організацій «Розвитку військово-спортивних багатоборства», президентом якого став <b>Зайцев С.В.</b> (Засновник бойового багатоборства, заслужений тренер України, суддя
+          міжнародної категорії, чорний пояс 10 дан, майстер спорту), першим віце-президентом став Федорцов А.П. - Засновник бойового багатоборства, суддя міжнародної категорії, чорний пояс 8 дан), другим віце-президентом став Самсонов В.
+        </p>
+      </div>
+
+      <div class="col-md-6 wow fadeInDown justif" data-wow-duration="1000ms" data-wow-delay="600ms">
+        <h2><br></h2>
+        <p>Основна мета діяльності організації – військово-патріотичне виховання дітей та молоді, пропаганда здорового способу життя, підвищення громадянської активності дітей та молоді, забезпечення їх доступу до участі у державотворчих процесах, залучення
+          дітей та молоді до культурного життя України та сприяння проведенню державних, народних і традиційних свят за їх участю, сприяння розвитку лідерських якостей у дітей та молоді, сприяння підготовці молоді до служби у лавах Збройних Сил України.</p>
+        <p>Спортсмени, які займаються цим видом спорту, мають можливість брати участь:
+          <br> - У поясний атестації міжнародної Федерації
+          <br> - У районних, обласних, національних, міжнародних змаганнях та отримувати відповідні <b>спортивні розряди</b> і <b>звання</b>
+          <br> - У літніх спортивних таборах
+          <br> - У військово-патріотичних іграх
+          <br>
+        </p>
+        <p>ВФВСБ на даний момент підготовлені:
+          <br> - Заслужених тренерів України - 9 осіб;
+          <br> - Майстрів спорту міжнародного класу - 20 осіб;
+          <br> - Майстрів спорту України - 200 осіб;
+          <br> - І велика кількість спортсменів розрядників.
+          <br>
+        </p>
+        <p>В процесі роботи ВФВСБ були підготовлені:
+          <br> - 7 суддів міжнародної категорії;
+          <br> - 5 суддів Національної категорії;
+          <br> - І більше 150 суддів 1-3 їх категорій.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="lates">
+    <div class="container">
+      <div class="text-center">
+        <h2>Останні Новини</h2>
+      </div>
+      <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <img src="images/news/1.png" class="img-responsive" />
+        <h3>Чемпіонат України 2017 року</h3>
+        <p>З 12.05.2017 по 14.05.2107 в м.Харків проводився Чемпіонат України з військово-спортивних багатоборств, розділ "Бойове двоборство".
+        </p>
+      </div>
+
+      <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <img src="images/news/1.png" class="img-responsive" />
+        <h3>Чемпіонат України 2017 року</h3>
+        <p>З 12.05.2017 по 14.05.2107 в м.Харків проводився Чемпіонат України з військово-спортивних багатоборств, розділ "Бойове двоборство".
+        </p>
+      </div>
+
+      <div class="col-md-4 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <img src="images/news/1.png" class="img-responsive" />
+        <h3>Чемпіонат України 2017 року</h3>
+        <p>З 12.05.2017 по 14.05.2107 в м.Харків проводився Чемпіонат України з військово-спортивних багатоборств, розділ "Бойове двоборство".
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <section id="partner">
+    <div class="container">
+      <div class="center wow fadeInDown">
+        <h2>Наші підрозділи</h2>
+        <p>В даний час ВФВСБ має свої структурні підрозділи в 15 областях України.</p>
+        <br>
+        <br>
+      </div>
+
+      <div class="partners">
+        <ul style="text-align: left" class="regions">
+          <li>
+            - <a href="#">Київська область</a>
+            <br> - <a href="#">Житомирська область</a>
+            <br> - <a href="#">Львівська область </a>
+            <br> - <a href="#">Чернівецька область </a>
+            <br>
+            <!-- <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms" src="images/partners/partner1.png"></a> -->
+          </li>
+          <li>
+            - <a href="#">Черкаська область </a>
+            <br> - <a href="#">Харківська область</a>
+            <br> - <a href="#">Дніпропетровська область</a>
+            <br> - <a href="#">Хмельницька область</a>
+            <br>
+            <!-- <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" src="images/partners/partner2.png"></a> -->
+          </li>
+          <li>- <a href="#">Херсонська область</a>
+            <br> - <a href="#">Одеська область</a>
+            <br> - <a href="#">Кіровоградська область  </a>
+            <br> - <a href="#">Луганська область </a>
+            <br>
+            <!-- <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms" src="images/partners/partner3.png"></a></li> -->
+          </li>
+          <li>- <a href="#">Запорізька область</a>
+            <br> - <a href="#">Полтавська область</a>
+            <br> - <a href="#">Кіровоградська область</a>
+            <br> - <a href="#">Луганська область </a>
+            <br>
+            <!-- <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1200ms" src="images/partners/partner4.png"></a> -->
+          </li>
+          <li>
+            - <a href="#">Херсонська область</a>
+            <br> - <a href="#">Одеська область</a>
+            <br> - <a href="#">Кіровоградська область </a>
+            <br> - <a href="#">Луганська область</a>
+            <br>
+            <!-- <a href="#"><img class="img-responsive wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="1500ms" src="images/partners/partner5.png"></a> -->
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!--/.container-->
+  </section>
+  <!--/#partner-->
+
+  <section id="conatcat-info">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <div class="center wow fadeInDown">
+            <h2>Анонс подій</h2>
+            <div class="tt-panel" style="display: block;">
+              <ul class="skr">
+                <li class='tcarusel-item'>
+
+                  <img class='news_img' src="images/events/1.jpg" alt='%s' tooltip='%s'>
+                  <a href='events.php?id=%s&page=1'><b>Дата</b> // Місто //   <br>Заголовок події 1
+</a>
+                </li>
+                <li class='tcarusel-item'>
+                  <a href='events.php?id=%s&page=1'>
+                    <img class='news_img' src='images/events/1.jpg' alt='%s' tooltip='%s'>
+                    <br>
+                    <b>Дата</b> // Місто //
+                    <br>Заголовок події 1
+                  </a>
+                </li>
+                <li class='tcarusel-item'>
+                  <a href='events.php?id=%s&page=1'>
+                    <img class='news_img' src='images/events/1.jpg' alt='%s' tooltip='%s'>
+                    <br>
+                    <b>Дата</b> // Місто //
+                    <br>Заголовок події 1
+                  </a>
+                </li>
+                <li class='tcarusel-item'>
+                  <a href='events.php?id=%s&page=1'>
+                    <img class='news_img' src='images/events/1.jpg' alt='%s' tooltip='%s'>
+                    <br>
+                    <b>Дата</b> // Місто //
+                    <br>Заголовок події 1
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-5">
+          <div class="center wow fadeInDown">
+            <h2>Контакти</h2>
+            <ul class="contacts">
+              <li>Президент федерації: Зайцев Сергій Васильович</li>
+              <li>тел.: ххх - ххх - ххх</li>
+              <li>email: email@ukr.net</li>
+              <li>адреса: м. Харків</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--/.container-->
+  </section>
+  <!--/#conatcat-info-->
+
+  <footer>
+   <? include("blocks/footer.php"); ?>
+  </footer>
+
+</body>
+</html>
