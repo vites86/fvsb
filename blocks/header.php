39965f6dbf3c7b3dddebcba0e9de5c4b1893d9fe
Victor Dubrovin <vites@outlook.com>
2017-05-27 17:08:06 +0300

    init

---
+++ blocks/header.php
@@ -0,0 +1,50 @@
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="navigation">
+		<div class="container">					
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse.collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div class="navbar-brand">
+					<!-- <a href="index.html"><h1><span>Військово-спортивні</span> багатоборства</h1></a> -->
+				</div>
+			</div>
+			
+			<div class="navbar-collapse collapse">							
+				<div class="menu">
+					<ul class="nav nav-tabs" role="tablist">
+						<li role="presentation"><a id="header_index"     href="index.php">Головна</a></li>
+						<li role="presentation"><a id="header_about"     href="about.php">Про нас</a></li>
+						<li role="presentation"><a id="header_units"     href="units.php">Підрозділи</a></li>								
+						<li role="presentation"><a id="header_portfolio" href="portfolio.php">Медіа</a></li>
+						<li role="presentation"><a id="header_blog"      href="blog.php">Документи</a></li>
+						<li role="presentation"><a id="header_contact"   href="contact.php">Контакти</a></li>						
+						<li role="presentation"><a id="header_anons"     href="anons.php">Анонс</a></li>						
+						<li role="presentation"><a id="header_news"      href="news.php">Новини</a></li>						
+					</ul>
+					<script>
+        $(window).load(function(){   
+           document.getElementById("header_index").className = '';
+           document.getElementById("header_about").className = '';
+           document.getElementById("header_units").className = '';
+           document.getElementById("header_portfolio").className = '';
+           document.getElementById("header_blog").className = '';
+           document.getElementById("header_contact").className = '';
+           document.getElementById("header_anons").className = '';
+           document.getElementById("header_news").className = '';  
+});
+      </script>
+});
+      </script>
+				</div>
+			</div>						
+		</div>
+	</div>	
+</nav>		
+
+
+
+
---
+++ blocks/header_links.php
@@ -0,0 +1,26 @@
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link href="css/prettyPhoto.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet" />
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">	
+    <!-- =======================================================
+        Theme Name: Company
+        Theme URL: https://bootstrapmade.com/company-free-html-bootstrap-template/
+        Author: BootstrapMade
+        Author URL: https://bootstrapmade.com
+    ======================================================= -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-2.1.1.min.js"></script>  
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>  
+    <script src="js/wow.min.js"></script>
+    <script src="js/functions.js"></script>
