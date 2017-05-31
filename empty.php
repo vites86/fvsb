4703e8bf9ded775bf862ed4fc04e63970f5a81d1
Viktor <vites@outlook.com>
2017-05-31 16:04:36 +0300

    plus media

---
+++ empty.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include("blocks/header_links.php"); ?>  
+    <title>Підрозділи</title>   
+  </head>
+  <body>
+	<header>		
+		  <?php include("blocks/header.php"); ?>
+	<script>
+        $(window).load(function(){   
+           document.getElementById("header_media").className = 'active';
+        });
+      </script>
+	</header>
+	
+	<div id="breadcrumb">
+		<div class="container">	
+			<div class="breadcrumb">							
+				<li><a href="index.html">Головна</a></li>
+				<li>Медіа</li>			
+			</div>		
+		</div>	
+	</div>	
+    
+    <div class="services">
+		<div class="container">
+			<h3>Київська область</h3>
+			<hr>
+			<div class="col-md-3">
+				<img src="images/units/kv/1.png" class="img-responsive" style="width:100%">
+			</div>
+			
+			<div class="col-md-9">
+				<div class="media">
+					<ul>
+                        <li>
+							<div class="media-body">
+								<h5 class="media-heading">Голова:</h5>
+							</div>
+						</li>
+                        <li>
+							<div class="media-left">
+								<i class="fa fa-user"></i>						
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading">Пристінський Олександр Васильович</h4>
+							</div>
+						</li>
+						<li>
+							<div class="media-left">
+								<i class="fa fa-pencil"></i>						
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading">(097) 405 31 93</h4>
+							</div>
+						</li>
+						<li>
+							<div class="media-left">
+								<i class="fa fa-envelope-o"></i>						
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading">fvsb.kiev@ukr.net</h4>
+							</div>
+						</li>
+						<li>
+							<div class="media-left">
+								<i class="fa fa-globe"></i>						
+							</div>
+							<div class="media-body">
+								<h4 class="media-heading"><a href="http://fvsb.kiev.ua" target="_blank">fvsb.kiev.net.ua</a></h4>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>	
+	
+	<div class="sub-services">		
+	</div>
+	
+	
+	<footer>
+		   <? include("blocks/footer.php"); ?>
+	</footer>
+	
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery-2.1.1.min.js"></script>	
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>  
+	<script src="js/wow.min.js"></script>
+	<script src="js/functions.js"></script>
+    
+</body>
+</html>
---
+++ media.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include("blocks/header_links.php"); ?>  
+    <title>Підрозділи</title>   
+  </head>
+  <body>
+	<header>		
+		  <?php include("blocks/header.php"); ?>
+	<script>
+        $(window).load(function(){   
+           document.getElementById("header_media").className = 'active';
+        });
+      </script>
+	</header>
+	
+	<div id="breadcrumb">
+		<div class="container">	
+			<div class="breadcrumb">							
+				<li><a href="index.html">Головна</a></li>
+				<li>Медіа</li>			
+			</div>		
+		</div>	
+	</div>	
+	
+	<section id="portfolio">	
+        <div class="container">
+            <div class="center">
+               <p>Пропунуємо Вашій увазі збірку медіа-файлів Всеукраїнської Федерації Військово-спортивних багатоборств</p>
+            </div>
+
+            <ul class="portfolio-filter text-center">
+                <li><a class="btn btn-default active" href="#" data-filter="*">Усі файли</a></li>
+                <li><a class="btn btn-default" href="#" data-filter=".bootstrap">Відео</a></li>
+                <li><a class="btn btn-default" href="#" data-filter=".html">Фото</a></li>
+                <li><a class="btn btn-default" href="#" data-filter=".wordpress">Інше</a></li>
+            </ul><!--/#portfolio-filter-->
+		</div>
+		<div class="container">
+            <div class="">
+                <div class="portfolio-items">
+                    <div class="portfolio-item apps col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/item1.png" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Business theme</a></h3>
+                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                    <a class="preview" href="images/portfolio/full/item1.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/item2.png" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Business theme</a></h3>
+                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                    <a class="preview" href="images/portfolio/full/item2.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>          
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item bootstrap wordpress col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/item3.png" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Business theme</a></h3>
+                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                    <a class="preview" href="images/portfolio/full/item3.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>        
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item joomla wordpress apps col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/item4.png" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Business theme</a></h3>
+                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                    <a class="preview" href="images/portfolio/full/item4.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>           
+                    </div><!--/.portfolio-item-->
+          
+                    <div class="portfolio-item joomla html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/item5.png" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Business theme</a></h3>
+                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                    <a class="preview" href="images/portfolio/full/item5.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>      
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item wordpress html apps col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/item6.png" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Business theme</a></h3>
+                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                    <a class="preview" href="images/portfolio/full/item6.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>         
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item wordpress html col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/item7.png" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Business theme</a></h3>
+                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                    <a class="preview" href="images/portfolio/full/item7.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>          
+                    </div><!--/.portfolio-item-->
+
+                    <div class="portfolio-item wordpress html bootstrap col-xs-12 col-sm-4 col-md-3">
+                        <div class="recent-work-wrap">
+                            <img class="img-responsive" src="images/portfolio/recent/item8.png" alt="">
+                            <div class="overlay">
+                                <div class="recent-work-inner">
+                                    <h3><a href="#">Business theme</a></h3>
+                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority</p>
+                                    <a class="preview" href="images/portfolio/full/item8.png" rel="prettyPhoto"><i class="fa fa-eye"></i> View</a>
+                                </div> 
+                            </div>
+                        </div>          
+                    </div><!--/.portfolio-item-->
+                </div>
+            </div>
+        </div>
+    </section><!--/#portfolio-item-->	
+
+	<footer>
+		   <? include("blocks/footer.php"); ?>
+	</footer>
+		   <? include("blocks/footer_links.php"); ?>  
+    
+</body>
+</html>
