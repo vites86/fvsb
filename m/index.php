080154a46347c11f37cb34fe7c828a2b4f60db6b
Victor Dubrovin <vites@outlook.com>
2017-09-02 17:42:09 +0300

    response

---
+++ m/index.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>ВСБ</title>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="../css/font-awesome.min.css">
+	<link rel="stylesheet" href="../css/animate.css">
+	<link href="../css/prettyPhoto.css" rel="stylesheet">
+	<link href="../css/style.css" rel="stylesheet" />
+	<link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../images/favicon.ico" type="image/x-icon">	
+    <!-- =======================================================
+        Theme Name: Company
+        Theme URL: https://bootstrapmade.com/company-free-html-bootstrap-template/
+        Author: BootstrapMade
+        Author URL: https://bootstrapmade.com
+    ======================================================= -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../js/jquery-2.1.1.min.js"></script>  
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/jquery.prettyPhoto.js"></script>
+    <script src="../js/jquery.isotope.min.js"></script>  
+    <script src="../js/wow.min.js"></script>
+    <script src="../js/functions.js"></script>
+</head>
+<body>
+  <header>
+      <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="navigation">
+		<div class="container">					
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse.collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div class="navbar-brand">
+					<a href="index.html"><h1><span></span></h1></a> 
+				</div>
+			</div>
+			
+			<div class="navbar-collapse collapse">							
+				<div class="menu">
+					<ul class="nav nav-tabs" role="tablist">
+						<li role="presentation"><a id="header_index"     href="../index.php">Головна</a></li>
+						<li role="presentation"><a id="header_about"     href="../about.php">Про нас</a></li>
+						<li role="presentation"><a id="header_units"     href="../units.php">Підрозділи</a></li>	
+						<li role="presentation"><a id="header_docs"      href="../docs.php">Документи</a></li>
+						<li role="presentation"><a id="header_news"      href="../news.php">Новини</a></li>							
+						<li role="presentation"><a id="header_media"     href="../media.php">Фото</a></li>
+						<li role="presentation"><a id="header_events"     href="../events.php">Події</a></li>						
+						<li role="presentation"><a id="header_contact"   href="../contacts.php">Контакти</a></li>						
+					</ul>					
+				</div>
+			</div>						
+		</div>
+	</div>	
+</nav>		
+      <script>                                
+               $(window).load(function(){   
+                  document.getElementById("header_index").className = 'active';          
+             }); 
+      </script>
+  </header>
+  <section id="main-slider" class="no-margin">
+    <div class="carousel slide">
+      <div class="carousel-inner">
+        <div class="item active" style="background-image: url(../images/slider/bg1.jpg)">
+          <div class="container">
+            <div class="row slide-margin">
+              <div class="col-sm-12">
+                <div class="carousel-content" style="text-align:center !important;text-shadow: grey 1px 2px 3px;">
+                  <p class="animation animated-item-2">громадська організація</p>
+                  <h2 class="animation animated-item-1">Всеукраїнська <span>Федерація</span></h2>
+                  <p class="animation animated-item-2"style="color:white !important; ">військово-спортивних багатоборств</p>
+                  <img style="width:30%;" src="../images/logo.png" />
+                  <br>
+                  <br>
+                  <span id='status'>
+                     08.10.2010 року ВФВСБ наказом № 3531 Міністерства України у справах молоді, сім'ї та спорту присвоєно статус <b>«Національної»</b>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--/.item-->
+      </div>
+      <!--/.carousel-inner-->
+    </div>
+    <!--/.carousel-->
+  </section>
+  <!--/#main-slider-->
+<div class="row">
+         <div class="col-md-12 text-center">
+           <span style="color: black; margin-top: 25px;">"
+              <b> Військово-спортивні багатоборства"</b> (далі ВСБ) - це спортивно-технічний і прикладний вид спорту, 
+              створений в кінці 90-х років в Україні. 
+              Входить до "Переліку видів спорту, що визнані в Україні" та є неоліпійським видом спорту.
+              <a href="https://uk.wikipedia.org/wiki/Військово-спортивні_багатоборства">(Вікіпедія)</a>
+           </span>
+         </div>
+      </div>
+  <div class="feature">
+    <div class="container">
+      <div class="text-center">   
+            <div>
+                <span style="color: black; margin-top: 20px; font-weight:bold;">Включає в себе:</span>
+            </div>
+            <h2>Військово-прикладне семиборствo:</h2>
+            <ul type="none" style="color:black; text-align: left; margin-left: 100px;">
+              <li>- плавання 50 метрів</li>
+              <li>- стрільба</li>
+              <li>- біг на 100 та 3000 м</li>
+              <li>- підтягування на перекладені</li>
+              <li>- подолання смуги перешкод</li>
+              <li>- метання імітаційних гранат</li>
+            </ul>
+
+            <h2>Бойове двоборство:</h2>
+            <ul type="none" style="color:black; text-align: left; margin-left: 100px;">
+              <li>- Всестильовий бій (володіння прийомами за допомогою ударної і 
+                     кидкової техніки з використанням елементів боротьби, боксу та східних 
+                     єдиноборств (карате та ін.)).</li>
+              <li>- Стрільба з пневматичної гвинтівки.</li>             
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="about">
+    <div class="container">
+      <div class="col-md-12 wow fadeInDown justif" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <h2 style="text-align:center">Про нас</h2>
+        <img src="../images/world.png" class="img-responsive" />
+        <p> Громадська організація "Всеукраїнська Федерація військово-спортивних багатоборств" <b>(ВФВСБ)</b> створена 8 лютого 2000 року 
+            на базі Федерації військово-прикладного спорту Харківської області (зареєстрована 31.01.1997).
+        </p>
+        <p> 08.10.2010 року ВФВСБ наказом № 3531 Міністерства України у справах молоді, сім'ї та спорту присвоєно статус <b>«Національної»</b>. 
+            В подальшому статус «Національної» був продовжений згідно наказу Міністерства молоді та спорту України №2385 від 18.07.2014 року.
+        </p>
+        <p> У 2005 році ВФВСБ стала одним із засновників Міжнародного Союзу громадських організацій «Розвитку військово-спортивних багатоборств», 
+            президентом якого став <b>Зайцев С.В.</b> (Засновник бойового багатоборства, заслужений тренер України, суддя
+            міжнародної категорії, чорний пояс 10 дан, майстер спорту), першим віце-президентом став Федорцов А.П. - 
+            Засновник бойового багатоборства, суддя міжнародної категорії, чорний пояс 8 дан.
+        </p>
+        <p> Основна <b>мета</b> діяльності організації – задоволення та захист законних соціальних, економічних, творчих, спортивних та інших спільних 
+            інтересів членів Організації та сприяння розвитку військово-спортивних багатоборств на території України, підвищенню ролі цього виду 
+            спорту у всебічному і гармонійному розвитку особистості, зміцнення здоров'я, формування здорового способу життя. Військово-патріотичне 
+            виховання дітей та молоді, пропаганда здорового способу життя і сприяння підготовці молоді до служби у лавах Збройних Сил України.
+        </p>     
+            
+        <p style="text-align:left;"> Спортсмени, які займаються цим видом спорту, мають можливість брати участь:
+        <br> - У технічній поясовій атестації згідно <a style="color:darkblue;text-decoration:underline" href="../documents/mezhdunarodnoe_polozhenie_o_tpa_utverzhdenoe_15.06.2011.doc">положення</a> відповідної міжнародної організаціїї 
+        <br> - У районних, обласних, національних, міжнародних змаганнях та отримувати відповідні <b>спортивні розряди</b> і <b>звання</b>
+        <br> - У літніх спортивних таборах
+        <br> - У військово-патріотичних заходах
+        <br>
+        </p>        
+        <table class="">    
+            <tr>
+                 <td colspan="2"><span style="color:black; font-weight:bold;">ВФВСБ на даний момент підготовлені:</span></td>
+            </tr>      
+             <tr>
+               <td style="padding-right:3px;width:55%;">
+                 <p style="margin-bottom:0px !important;">
+                     - Заслужених тренерів України - 9 осіб;
+                    <br> - Майстрів спорту міжнародного класу - 20 осіб;
+                    <br> - Майстрів спорту України - 200 осіб;
+                    <br> - Велика кількість спортсменів розрядників.                 
+                    <br> - Суддів міжнародної категорії - 10 осіб;
+                    <br> - Суддів Національної категорії - 9 осіб;
+                    <br> - Суддів 1-3 категорій - більше 150 осіб.
+                </p>
+               </td>
+             </tr>
+        </table>
+        <br>
+        <p>Правила спортивних змагань з військово-спортивних багатоборств затверджені <a style="color:blue" target="_blank" href="http://zakon2.rada.gov.ua/laws/show/z0504-14">Наказом</a> від 22.04.2014  № 1283 Міністерства молоді та спорту</p>
+        <hr>
+        <div style="position:relative;height:0;padding-bottom:56.25%">
+          <iframe src="https://www.youtube.com/embed/ja5MK4A1SxU?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen>
+          </iframe>
+        </div><br>
+      <p>
+          Спортивні звання та розряди з військово-спортивних багатоборств присвоюються відповідно до <a style="color:blue" target="_blank" href="http://zakon2.rada.gov.ua/laws/show/z0497-14">Наказу</a> №1305 Міністерства молоді та спорту України від 24.04.14 
+          "Про затвердження Кваліфікаційних норм та вимог Єдиної спортивної класифікації України з неолімпійських видів спорту"
+      </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="lates">
+    <div class="container" style="text-align:center">
+      <div class="text-center">
+        <h2>Останні Новини</h2>
+      </div>
+      <? 
+       include('../blocks/db.php');
+       if ($result = mysqli_query($db, 'SELECT * FROM news ORDER BY date_ desc limit 3')) 
+	     {
+         while( $myrow = mysqli_fetch_assoc($result) )
+	       {
+           printf("
+           <div class='col-sm-12'>
+             <a target='_blank' href='news_one.php?id=%s'>
+               <img src='../images/news/%s.png' class='img-responsive' style='width:100%%;'/>
+               <h3>%s</h3>
+               <p>%s</p>
+               <input type='button' value='Детальніше' 
+               class='btn-slide animation animated-item-3 btn btn-success' href='news_one.php?id=%s'></input>               
+             </a>        
+           </div>", $myrow['id'], $myrow['id'], $myrow['title'], $myrow['description'], $myrow['id']);
+               } 
+             mysqli_free_result($result); 
+	       }
+	       mysqli_close($db); 
+?>
+    </div>
+  </div>
+
+  <section id="partner">
+    <div class="container">
+      <div class="center">
+        <h2>Наші підрозділи</h2>
+        <p>В даний час ВФВСБ має свої структурні підрозділи в 17 областях України.</p>
+        <br>
+        <br>
+      </div>
+
+      <div class="partners">
+        <ul style="text-align: left" class="regions">
+        <li>
+            - <a href="units.php#tnp">Тернопільська область</a>
+            <br> - <a href="units.php#chk">Черкаська область</a>
+            <br> - <a href="units.php#lv">Львівська область</a>            
+            <br> - <a href="units.php#od">Одесська область</a>           
+            <br> - <a href="units.php#lg">Луганська область </a>
+            <br> - <a href="units.php#khv">Харьківська область</a>
+            <br> - <a href="#">Донецька область</a>
+            <br> - <a href="units.php#khm">Хмельницька область</a>
+            <br> - <a href="units.php#dnr">Дніпропетровська область</a>
+            <br> - <a href="units.php#sm">Сумська область</a>
+            <br> - <a href="#">Чернігівська область</a>
+            <br> - <a href="units.php#krg">Кропивницька область</a>
+            <br> - <a href="units.php#khs">Херсонська область</a>
+            <br> - <a href="units.php#zht">Житомирська область</a>
+            <br> - <a href="units.php#pl">Полтавська область </a>
+            <br> - <a href="units.php#zp">Запорізька область</a>
+            <br> - <a href="units.php#kv">Київська область </a>
+            <br>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!--/.container-->
+  </section>
+  <!--/#partner-->
+
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="center">
+              <br>
+            <h2>Анонс подій</h2>
+            <div class="tt-panel" style="display: block;text-align:center">             
+                <? 
+                     include('../blocks/db.php');
+                     if ($result = mysqli_query($db, "SELECT *, DATE_FORMAT(date_,'%d.%m.%Y') as eurodate FROM events where date_ > NOW() ORDER BY date_")) 
+	                   {
+                       while( $myrow = mysqli_fetch_assoc($result) )
+	                     {
+                         printf("  
+                         <div class='col-sm-12'>
+                           <a target='_blank' href='events_one.php?id=%s'>
+                             <img src='../images/events/%s.png' class='img-responsive' style='width:100%%;'/>
+                             <h3>%s</h3>
+                             <p>%s</p>
+                             <p>%s</p>
+                             <input type='button' value='Детальніше' 
+                             class='btn-slide animation animated-item-3 btn btn-success' href='events_one.php?id=%s'></input>               
+                           </a>   <hr/>     
+                         </div><br>
+                         ", $myrow['id'], $myrow['id'], $myrow['title'], $myrow['eurodate'], $myrow['place'], $myrow['id']);
+                             } 
+                           mysqli_free_result($result); 
+	                     }
+	                    //  mysqli_close($db); 
+                ?>             
+
+              </ul>
+            </div>
+          </div>
+        </div>
+        </div>
+    </div>
+    <!--/.container-->
+  </section>
+  <!--/#conatcat-info-->
+
+            <div class="container">
+               <div class="row">
+                 <div class="col-sm-12 center">
+                   <div class="center">
+                     <h2>Контакти</h2>                     
+                     <ul type="none" style="text-align: left; color:black;" class="regions" >
+                       <li>Президент федерації: Зайцев Сергій Васильович</li>
+                       <li><img src="../images/contact.png" style="width:20%;" alt=""></li>
+                       <li><b>тел.</b>: (063) 119 57 15</li>
+                       <li><b>email</b>: president.fvsb@gmail.com</li>
+                       <li><b>адреса</b>: м. Харків</li>
+                     </ul>
+                   </div>
+                 </div>
+              </div>
+            </div>
+
+
+  <footer>
+   <? include("../blocks/footer.php"); ?>
+  </footer>
+
+</body>
+</html>
